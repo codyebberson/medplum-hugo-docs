12999fd673c4c9edafb01e84235bc1f7c21064db
Cody Ebberson <cody@ebberson.com>
2026-02-12 12:26:09 -0800

    Sidebar update
    Signed-off-by: Cody Ebberson <cody@ebberson.com>

--- assets/js/main.mjs
+++ assets/js/main.mjs
@@ -1,6 +1,6 @@
-import mermaid from "mermaid";
-import Swup from "swup";
-import { initAnimatedCircles } from "./components/animated-circle.mjs";
+import mermaid from 'mermaid';
+import Swup from 'swup';
+import { initAnimatedCircles } from './components/animated-circle.mjs';
 
 mermaid.initialize({ startOnLoad: true });
 
@@ -9,25 +9,66 @@ function setAttribute(key, value) {
 }
 
 function updateAttributes(newUrl) {
-  const onDocsPage = newUrl.includes("/docs/");
-  setAttribute("breadcrumbs", onDocsPage ? "visible" : "hidden");
-  setAttribute("sidebar", onDocsPage ? "open" : "closed");
-  setAttribute("toc", onDocsPage ? "visible" : "hidden");
-  setAttribute("docFooter", onDocsPage ? "visible" : "hidden");
+  const onDocsPage = newUrl.includes('/docs/');
+  setAttribute('breadcrumbs', onDocsPage ? 'visible' : 'hidden');
+  setAttribute('sidebar', onDocsPage ? 'open' : 'closed');
+  setAttribute('toc', onDocsPage ? 'visible' : 'hidden');
+  setAttribute('docFooter', onDocsPage ? 'visible' : 'hidden');
 }
 
+function updateActiveLinks() {
+  document.querySelectorAll('.sidebar-category-header-wrapper.active').forEach((el) => {
+    el.classList.remove('active');
+  });
+
+  document.querySelectorAll('.sidebar-link.active').forEach((el) => {
+    el.classList.remove('active');
+  });
+
+  document.querySelectorAll('.sidebar-link[href]').forEach((link) => {
+    if (link.href === window.location.href) {
+      link.closest('.sidebar-category-header-wrapper')?.classList.add('active');
+      link.classList.add('active');
+    }
+  });
+}
+
+document.body.addEventListener('click', (event) => {
+  const target = event.target;
+
+  if (target.matches('.sidebar-caret')) {
+    console.log('Toggle sidebar category:', target);
+    const category = target.closest('.sidebar-category');
+    if (category) {
+      category.classList.toggle('expanded');
+    }
+  }
+
+  if (target.matches('.sidebar-category-link')) {
+    const category = target.closest('.sidebar-category');
+    if (category) {
+      if (target.href === window.location.href) {
+        category.classList.toggle('expanded');
+      } else {
+        category.classList.add('expanded');
+      }
+    }
+  }
+});
+
 export const swup = new Swup({
-  containers: ["#swup"],
+  containers: ['#swup'],
   animationSelector: false,
   animateHistoryBrowsing: false,
 });
 
-swup.hooks.before("content:replace", (visit) => {
+swup.hooks.before('content:replace', (visit) => {
   updateAttributes(visit.to.url);
 });
 
-swup.hooks.on("content:replace", (visit) => {
+swup.hooks.on('content:replace', (visit) => {
   initAnimatedCircles();
+  updateActiveLinks();
 });
 
 updateAttributes(window.location.href);
